7898d3e49a6eb81d2efee979e227495aa9bbb81e
kkno2000 <3376465612@qq.com>
2022-03-02 17:38:58 +0800

    二分法查找

---
+++ binary_seaarch.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main7() {
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int m = 7;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int left = 0;
+	int right = sz - 1;
+	while (left <= right)
+	{
+		int mid = (left + right) / 2;
+		if (arr[mid] > m) {
+			right = mid - 1;
+		}
+		else if (arr[mid] < m) {
+			left = mid + 1;
+		}
+		else
+		{
+			printf("找到了，下标是：%d", mid);
+			break;
+		}
+	}
+	if (left > right) {
+		printf("找不到");
+	}
+	return 0;
+}
