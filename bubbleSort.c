2efa6f035fc397b22fa096d9ce9dadf645eb52d4
kkno1 <98086166+kkno2000@users.noreply.github.com>
2022-04-03 18:11:35 +0800

    Add files via upload

---
+++ bubbleSort.c
@@ -0,0 +1,14 @@
+void bubbleSort(int arr[], int sz) //冒泡排序思想：两两相邻的元素进行比较，并且可能需要交换
+{
+	int i = 0;
+	for (i = 0; i < sz - 1; i++) {
+		int j = 0;
+		for (j = 0; j < sz - 1 - i; j++) {
+			if (arr[j] > arr[j + 1]) {
+				int tmp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = tmp;
+			}
+		}
+	}
+}
